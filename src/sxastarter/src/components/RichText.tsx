4e4559d1a7ab7f280ee812a33eb3dabad333946b
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2023-11-15 11:42:14 +1100

    Test

--- src/sxastarter/src/components/RichText.tsx
+++ src/sxastarter/src/components/RichText.tsx
@@ -52,8 +52,8 @@ export const Default = (props: RichTextProps): JSX.Element => {
   ) as RichTextField;
 
   return (
-    <ComponentRichText styles={props.params.styles}>      
-        <JssRichText field={field} />
+    <ComponentRichText styles={props.params.styles}>
+      <JssRichText field={field} />
     </ComponentRichText>
   );
 };
