2c61732e68fd0af7307fce65c67b848128f6c327
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2023-11-17 16:16:51 +1100

    RichText default version restored

--- src/sxastarter/src/components/RichText.tsx
+++ src/sxastarter/src/components/RichText.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import {
-  //Field,
-  useSitecoreContext,
-  RichText as JssRichText,
-  RichTextField,
-} from '@sitecore-jss/sitecore-jss-nextjs';
+import { Field, RichText as JssRichText } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
-  Text: RichTextField;
+  Text: Field<string>;
 }
 
 export type RichTextProps = {
@@ -15,45 +10,16 @@ export type RichTextProps = {
   fields: Fields;
 };
 
-type ComponentRichTextProps = {
-  styles: string;
-  children: JSX.Element;
-};
-
-const ComponentRichText = (props: ComponentRichTextProps) => {
-  return (
-    <div className={`component test-3 rich-text ${props.styles}`}>
-      <div className="component-content">{props.children}</div>
-    </div>
-  );
-};
-
 export const Default = (props: RichTextProps): JSX.Element => {
-  const { sitecoreContext } = useSitecoreContext();
   const text = props.fields ? (
     <JssRichText field={props.fields.Text} />
   ) : (
     <span className="is-empty-hint">Rich text</span>
   );
-  if (sitecoreContext.pageState === 'normal') {
-    return (
-      <div className={`component test-1 rich-text ${props.params.styles.trimEnd()}`}>
-        <div className="component-content">{text}</div>
-      </div>
-    );
-  }
-
-  const field = (
-    props.fields && props.fields.Text ? (
-      props.fields.Text
-    ) : (
-      <span className="is-empty-hint">Rich text</span>
-    )
-  ) as RichTextField;
 
   return (
-    <ComponentRichText styles={props.params.styles}>
-      <JssRichText field={field} />
-    </ComponentRichText>
+    <div className={`component rich-text ${props.params.styles.trimEnd()}`}>
+      <div className="component-content">{text}</div>
+    </div>
   );
 };
