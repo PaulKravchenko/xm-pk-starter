209ec6c77c75eed2ce9150e3bcb397edcf587b23
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2024-03-21 10:18:25 +1100

    Test User Api implemented

---
+++ src/platform/Controllers/Api/UserApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Sitecore.Security.Accounts;
+using Sitecore.SecurityModel;
+
+namespace XmCloudSXAStarter.Controllers.Api
+{
+    [RoutePrefix("api/users")]
+    public class UserApiController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetUsers()
+        {
+            var usersNames = new List<string>();
+
+            using (new SecurityDisabler())
+            {
+                var users = UserManager.GetUsers();
+                usersNames = users.Select(u => u.Name).ToList();
+            }
+
+            return Json(usersNames);
+        }
+    }
+}
